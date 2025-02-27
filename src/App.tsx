99870efa722d74ca840377445b3d21a728fb8aa0
Dmytro <144100405+Kukuliak-Dmytro@users.noreply.github.com>
2025-02-27 09:09:17 +0000

    Minimal code setup for CodeSandbox

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,45 @@
-import { Outlet } from 'react-router-dom'
-import { Link } from 'react-router-dom'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { usePrefetch } from './hooks/usePrefetch.ts';
-import fetchPosts from './services/postsFetches.ts';
-import fetchAlbums from './services/albumsFetches.ts'
-import { GlobalModal } from './components/common/Modal/Modal.tsx'
-import { LoadingOverlay } from './components/common/OverlaySpinner/OverlaySpinner.tsx'
+import { Outlet } from "react-router-dom";
+import { Link } from "react-router-dom";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { usePrefetch } from "./hooks/usePrefetch.ts";
+import fetchPosts from "./services/postsFetches.ts";
+// import fetchAlbums from './services/albumsFetches.ts'
+import { GlobalModal } from "./components/common/Modal/Modal.tsx";
+import { LoadingOverlay } from "./components/common/OverlaySpinner/OverlaySpinner.tsx";
 function App() {
-  const prefetchPosts=usePrefetch(fetchPosts)
-  const prefetchAlbums=usePrefetch(fetchAlbums)
+  const prefetchPosts = usePrefetch(fetchPosts);
+  // const prefetchAlbums=usePrefetch(fetchAlbums)
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank" rel='noopener'>
+        <a href="https://vitejs.dev" target="_blank" rel="noopener">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank" rel='noopener'>
+        <a href="https://react.dev" target="_blank" rel="noopener">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
       <nav>
-        <Link to='/React-Query/' >Home</Link>
+        <Link to="/React-Query/">Home</Link>
         {" | "}
-        <Link to='/React-Query/posts'  onMouseEnter={()=>prefetchPosts(['posts'])}>Posts</Link>
-        {" |" }
-        <Link to='/React-Query/albums' onMouseEnter={(()=>prefetchAlbums(['albums']))}>Albums</Link>
-
+        <Link
+          to="/React-Query/posts"
+          onMouseEnter={() => prefetchPosts(["posts"])}
+        >
+          Posts
+        </Link>
+        {/* {" |" } */}
+        {/* <Link to='/React-Query/albums' onMouseEnter={(()=>prefetchAlbums(['albums']))}>Albums</Link> */}
       </nav>
-     <Outlet></Outlet>
-     <GlobalModal></GlobalModal>
-     <LoadingOverlay></LoadingOverlay>
-  
+      <Outlet></Outlet>
+      <GlobalModal></GlobalModal>
+      <LoadingOverlay></LoadingOverlay>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { Home } from "./pages/Home.tsx";
 import { Contact } from "./pages/Contact.tsx";
-import { Posts } from './pages/Posts.tsx';
-import { Albums } from './pages/Albums.tsx';
-import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
-import PostPage from './pages/PostPage.tsx'
-import AlbumPage from './pages/AlbumPage.tsx';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { Posts } from "./pages/Posts.tsx";
+// import { Albums } from './pages/Albums.tsx';
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import PostPage from "./pages/PostPage.tsx";
+// import AlbumPage from './pages/AlbumPage.tsx';
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 const queryClient = new QueryClient();
 
 const router = createBrowserRouter([
@@ -31,18 +31,17 @@ const router = createBrowserRouter([
         element: <Posts />,
       },
       {
-        path:"/React-Query/posts/:id",
-        element:<PostPage/>
+        path: "/React-Query/posts/:id",
+        element: <PostPage />,
       },
-      {
-        path:'/React-Query/albums',
-        element:<Albums/>
-      },
-      {
-        path:'/React-Query/albums/:id',
-        element:<AlbumPage/>
-      }
-
+      // {
+      //   path: "/React-Query/albums",
+      //   element: <Albums />,
+      // },
+      // {
+      //   path: "/React-Query/albums/:id",
+      //   element: <AlbumPage />,
+      // },
     ],
   },
 ]);
